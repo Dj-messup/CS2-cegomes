54c34947f7a78e9b2ffa38f7693a9e4fee74f287
“Chica <cegomes@mavs.coloradomesa.edu>
2024-09-03 14:33:27 -0600

    look at the stars

--- GameOfLife/Main.cpp
+++ GameOfLife/Main.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <string>
-#include "gameOfife.h"
+#include "gameOfLife.h"
 
 using namespace std;
 
-int main (int argc, char* argv[])
+int main(int argc, char* argv[])
 {
     int stage = 0;
     bool boardChanged = true;
-    size_t boardSize = 10; 
-    Cell* board [10][10];
+    size_t boardSize = 10;
+    Cell* board[10][10];
 
     initCells(board, boardSize);
-    readBoard(board, boardSize)
+    readBoard(board, boardSize);
 
-    while (boardChanged)
-    {
-        clearScreen()
-        cout <<"Stage: "<< stage << endl;
-        
+    while(boardChanged) {
+        clearScreen();
+        cout << "Stage: " << stage << endl;
+
         printCells(board, boardSize);
-        boardChanged = updateCellState (board, boardSize);
+        boardChanged = updateCellState(board, boardSize);
 
         cin.get();
         stage++;
     }
+
+    // Clean up board if cells were allocated on the heap
+
     return 0;
 }
