41d23cd7928bc03b61d4c300000da6670683c630
“Chica <cegomes@mavs.coloradomesa.edu>
2024-09-18 14:11:25 -0600

    Notes in class

--- PointDistance/point.h
+++ PointDistance/point.h
@@ -28,8 +28,21 @@ namespace points
             void setX(int);
             void setY(int);
             void setNearestPoint(Point*);
+            /*
+            This method takes in the array of pointers to Point 
+            and the array size. It returns a pointer to the Point that 
+            is nearest to this
+            */
 
             Point* calcNearestPoint(Point*[], unsigned long);
+
+            /*
+            This function takes in a Point and calculates 
+            the cartesian distance between the passed in Point
+            and this.
+
+            Returns the distance as a double. 
+            */
             double distPoints(Point&);
     };
 }
